287cc47dc60659373b242b1f3f3ccb9d4f0a8340
Adèle Desmazières <adele.desmazieres@inrae.fr>
2022-08-10 16:23:13 +0200

    centralisation des 3 blast dans un fichier

--- main.sh
+++ main.sh
@@ -5,11 +5,11 @@
 makeblastdb -in data/Prunus-persica-proteome.fasta -dbtype prot -out database/PP-db
 makeblastdb -in data/Malus-domestica-proteome.fasta -dbtype prot -out database/MD-db
 
-# TODO : modifier le script du blast pour que le path soit valide et pour qu'il réalise tous les blasts MD->PP MD->MD et PP->PP
 sbatch submit/blast_job.sh
 
 # TODO : s'assurer que le job donné à slurm termine avant de lancer les commandes suivantes
-cat results/iadhore/all/malus_vs_prunus.txt results/iadhore/all/malus_vs_malus.txt results/iadhore/all/prunus_vs_prunus.txt > results/iadhore/all/blast_all.txt
+#MD_vs_PP.txt results/blast/MD_vs_MD.txt results/blast/PP_vs_PP
+cat results/blast/*.txt > results/blast/blast_all.txt
 
 sbatch submit/iadhore_job.sh
 
