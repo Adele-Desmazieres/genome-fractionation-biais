979f7be689705fa03a00882b30745d474e24112c
Adèle Desmazières <adele.desmazieres@inrae.fr>
2022-08-17 14:36:49 +0200

    IMPORTANT : formater les données de sortie du blast pour iadhore

---
+++ main2.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+date # affiche la date et l'heure actuels
+
+TEST=$1 # si =1 lance les commandes sur les dossiers_test
+FORCED=$2 # si =1 force le lancement des commandes même si les résultats ont été calculés auparavant, écrase les résultats précédents
+
+# variables de chemins
+DATA="data"
+DB="database"
+RES="results"
+SUBMIT="submit"
+TMP="tmp"
+# si arg1 = 1 alors tous les dossiers doivent finir par _test sauf submit
+if [[ TEST -eq 1 ]]
+then
+    DATA="${DATA}_test"
+    DB="${DB}_test"
+    RES="${RES}_test"
+fi
+
+# supprime les résultats précédents si l'option forced vaut 1
+if [[ FORCED -eq 1 ]]
+then
+    rm -r $DB $RES
+fi
+
+# crée l'arborescence de dossiers si elle n'existe pas
+mkdir -p $DB $RES/{blast,iadhore,python}
+
+
+# crée les bases de données de blast de PP et MD si elles n'existent pas (si leur dossier est inexistant ou vide)
+# base de données de PP
+if [[ ! -d "$DB/PP-db/" || ! "$(ls -A $DB/PP-db/)" ]]
+then
+    printf "\n### MAKEBLASTDB PP ###"
+    makeblastdb -in "$DATA/Prunus-persica-proteome.fasta" -dbtype prot -out "$DB/PP-db/PP-db"
+    printf "makeblastdb PP: done\n"
+fi
+# base de données de MD
+if [[ ! -d "$DB/MD-db/" || ! "$(ls -A $DB/MD-db/)" ]]
+then
+    printf "\n### MAKEBLASTDB MD ###"
+    makeblastdb -in "$DATA/Malus-domestica-proteome.fasta" -dbtype prot -out "$DB/MD-db/MD-db"
+    printf "makeblastdb MD: done\n"
+fi
+
+
+# soumet le blast à slurm
+if [[ ! "$(ls -A $RES/blast)" ]]
+then
+    printf "\n### BLAST ###\n"
+    sbatch --wait --export=ALL,DATA=$DATA,DB=$DB,OUT="$RES/blast" $SUBMIT/blast_job.sh 
+    # --wait permet d'exit seulement quand le job termine, fait attendre le script sinon
+    # --export permet d'exporter des variables vers le script du job
+
+    # regroupe les 3 blast PP-PP MD-MD et MD-PP dans un même fichier
+    cat $RES/blast/*.txt > $RES/blast/all_vs_all0.txt
+    # traite le fichier pour ne conserver que les 2 premières colonnes, contenant les paires de gènes
+    cut -f1,2 $RES/blast/all_vs_all0.txt > $RES/blast/all_vs_all.txt
+
+    printf "blast: done\n"
+fi
+
+
+# soumet le iadhore job à slurm si dossier de résultats iadhore est vide
+if [[ ! "$(ls -A $RES/iadhore)" ]]
+then
+    printf "\n### IADHORE ###\n"
+    # déplace les fichiers d'entrée de DATA dans TMP/data pour que iadhore les choppe là-bas sans avoir à modifier son input path
+    if [[ -d $TMP ]]
+    then
+        rm -r $TMP
+    fi
+    mkdir -p $TMP/{blast,data,iadhore}
+    cp -r $RES/blast/* $TMP/blast/
+    cp -r $DATA/MD_lst $TMP/data/MD_lst
+    cp -r $DATA/PP_lst $TMP/data/PP_lst
+
+    # soumet le job
+    sbatch --wait $SUBMIT/iadhore_job.sh
+
+    # déplace les résultats de tmp vers le dossier de résultats
+    cp $TMP/iadhore/* $RES/iadhore/
+
+    # coupe la tabulation en trop dans le fichier multiplicon_pairs.txt si multiplicon_pairs.txt existe
+    if [ -f $RES/iadhore/multiplicon_pairs.txt ]
+    then
+        sed 's:\t\t*:\t:g' $RES/iadhore/multiplicon_pairs.txt > $RES/iadhore/multiplicon_pairs_modified.txt
+    fi
+    
+    printf "iadhore: done\n"
+fi
+
+
+# lance le script python
+printf "\n### PYTHON ###\n"
+python3 scripts/python/main2.py $TEST > $RES/python/fractionation_stat.txt 
+printf "python: done\n"
+
+date # affiche la date et l'heure actuels
+printf "\nscript: done\n"
